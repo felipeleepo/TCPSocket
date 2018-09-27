ba176881b4c3ff42ffb1855b01d6cad740f33743
Felipe Barbosa <felipebinfo@gmail.com>
2018-09-27 00:15:01 -0300

    Commit

---
+++ Cliente/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient cliente = new TcpClient();
+            cliente.Connect("127.0.0.1", 8001);
+            NetworkStream saida;
+            BinaryWriter escreve;
+            BinaryReader ler;
+            String x = "";
+
+            try
+            {
+
+                do
+                {
+                    saida = cliente.GetStream();
+
+                    escreve = new BinaryWriter(saida);
+                    ler = new BinaryReader(saida);
+                    String resposta = ler.ReadString();
+                    Console.WriteLine(resposta);
+                    Console.WriteLine("Envie para o Servidor:");
+                    x = Console.ReadLine();
+                    escreve.Write(x);
+
+                } while (x != "FIM");
+
+                Console.WriteLine("Encerrando Conexão. Clique para sair");
+                cliente.Close();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.ToString());
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Servidor/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPAddress local = IPAddress.Parse("127.0.0.1");
+
+                TcpListener servidor = new TcpListener(local, 8001);
+
+                Socket conexao;
+                NetworkStream socketStream;
+                BinaryWriter escrever;
+                BinaryReader ler;
+
+                servidor.Start();
+
+                Console.WriteLine("Servidor - " + servidor.LocalEndpoint);
+                bool enquanto = true;
+                while (enquanto)
+                {
+                    Console.WriteLine("Escutando...");
+                    conexao = servidor.AcceptSocket();
+
+                    socketStream = new NetworkStream(conexao);
+                    escrever = new BinaryWriter(socketStream);
+                    ler = new BinaryReader(socketStream);
+
+                    escrever.Write("Servidor - Conexão Efetuada");
+
+                    String conteudo = "";
+
+                    while (conteudo != "FIM" && conexao.Connected)
+                    {
+                        try
+                        {
+                            conteudo = ler.ReadString();
+                            Console.WriteLine("Cliente Mensagem: " + conteudo);
+                            escrever.Write("Servidor - 200 OK!");
+                        }
+                        catch (Exception)
+                        {
+                            break;
+                        }
+
+                    }
+                    escrever.Write("Servidor - Desligando...");
+                    escrever.Close();
+                    ler.Close();
+                    socketStream.Close();
+                    conexao.Close();
+                    Console.WriteLine("Conexão Finalizada!");
+                }
+
+                servidor.Stop();
+                Console.WriteLine("Servidor Foi Finalizado");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.ToString());
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
